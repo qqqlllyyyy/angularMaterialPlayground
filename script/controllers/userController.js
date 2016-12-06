cd9d4bc559ef620dae70b193f79723fd28b2c6ea
qqqlllyyyy <qinliyu1990@gmail.com>
2016-12-06 12:23:00 -0800

    Bottom sheet added

--- script/controllers/userController.js
+++ script/controllers/userController.js
@@ -35,50 +35,33 @@ Users.controller('UserController', function($mdBottomSheet, $mdSidenav, UserServ
 
 
 
-  function share($event) {
-
-    // $mdBottomSheet.show({
-    //   // controller: [$mdBottomSheet, UserSheetController],
-    //   controller: UserSheetController,
-    //   controllerAs: vm,
-    //   templateUrl: 'views/bottomsheet.html',
-    //   parent: angular.element(document.getElementById('content')),
-    // });
-    //
-    //
-    // function UserSheetController($mdBottomSheet) {
-    //   // this.user = selectedUser;
-    //   // this.items = [
-    //   //   { name: 'Phone'       , icon: 'phone'       , icon_url: ''},
-    //   //   { name: 'Twitter'     , icon: 'twitter'     , icon_url: ''},
-    //   //   { name: 'Google+'     , icon: 'google_plus' , icon_url: ''},
-    //   //   { name: 'Hangout'     , icon: 'hangouts'    , icon_url: ''},
-    //   // ];
-    //   // this.contactUser = function(action) {
-    //   // // The actually contact process has not been implemented...
-    //   // // so just hide the bottomSheet
-    //   //   $mdBottomSheet.hide(action);
-    //   // };
-    // }
+  function share(selectedUser) {
 
     $mdBottomSheet.show({
-      //template: '<md-bottom-sheet class="md-grid md-has-header"><md-subheader>Share</md-subheader><md-list><md-list-item ng-repeat="item in items"><md-button class="md-grid-item-content" ng-click="listItemClick($index)"><i class="item.icon"></i><div class="md-grid-text"> {{ item.name }} </div></md-button></md-list-item></md-list></md-bottom-sheet>',
+      controller: UserSheetController,
+      controllerAs: 'bottomCtrl',
       templateUrl: 'views/bottomsheet.html',
-      controller: GridBottomSheetCtrl,
-      targetEvent: $event
+      parent: angular.element(document.getElementById('content')),
     });
-    function GridBottomSheetCtrl($scope, $mdBottomSheet) {
-      // $scope.items = [
-      //   { icon: 'fa-twitter', class: 'md-primary md-hue-2' },
-      //   { icon: 'fa-google-plus', class: 'md-warn md-hue-2' },
-      //   { icon: 'fa-facebook', class: 'md-primary' },
-      //   { icon: 'fa-linkedin', class: 'md-primary' }
-      // ];
-      // $scope.listItemClick = function($index) {
-      //   var clickedItem = $scope.items[$index];
-      //   $mdBottomSheet.hide(clickedItem);
-      // };
+
+
+    function UserSheetController($mdBottomSheet) {
+
+      this.user = selectedUser;
+      this.items = [
+        { name: 'Phone'       , icon: 'phone-square'       , url: 'http://www.google.com'},
+        { name: 'Twitter'     , icon: 'twitter-square'     , url: 'http://www.twitter.com'},
+        { name: 'Google+'     , icon: 'google-plus-square' , url: 'http://www.google.com'},
+        { name: 'Git'         , icon: 'git-square'         , url: 'http://www.github.com'},
+      ];
+      this.contactUser = function(action) {
+      // The actually contact process has not been implemented...
+      // so just hide the bottomSheet
+        $mdBottomSheet.hide(action);
+      };
     }
+
+
 
   }
 
